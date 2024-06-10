244141fd4ec8ffe21857f51dd3cd47a7df4219ab
徳長和希 <tokunagakazuki@tokuchoukazukinoMacBook-Air.local>
2024-06-10 15:55:54 +0900

    dev

--- rlgtsjp.js
+++ rlgtsjp.js
@@ -1,17 +1,18 @@
 function preinitializeProgressier(){
     let pathname = window.location.pathname;
-    let startUrl = "";
-    if (pathname.startsWith("/manage/agent")){
-      startUrl = "manage/agent";
+    let config = {};
+    if (pathname.includes("/manage/agent") || pathname.includes("/manage/admin")){
+      config.startUrl = window.location.pathname.slice(1, window.location.pathname.length);
+      config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
     }
-    else if (pathname.startsWith("/manage/admin")){
-      startUrl = "manage/admin";
+    if (pathname.includes("/version-test")){
+      config.name = "[DEV] REALGETs";
+      config.shortName = "[DEV] REALGETs";
     }
-    window.progressierAppRuntimeSettings = {
-      startUrl: startUrl,
-    } 
+    window.progressierAppRuntimeSettings = config;
     let progressierScript = document.createElement("script");
     progressierScript.setAttribute("src", "https://progressier.app/1zd2mFNRlQRP9IOZW0hI/script.js");
     document.querySelector("head").appendChild(progressierScript);
   }
   preinitializeProgressier();
+  
