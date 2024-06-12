a537899349116b1273694f70c2ca3f6ddff82310
徳長和希 <tokunagakazuki@tokuchoukazukinoMacBook-Air.local>
2024-06-12 19:05:24 +0900

    test

--- bizpwa.js
+++ bizpwa.js
@@ -1,7 +1,7 @@
 function preinitializeProgressier(){
   let pathname = window.location.pathname;
   let config = {};
-  if (pathname.includes("/manage/agent") || pathname.includes("/manage/admin")){
+  if (pathname.includes("/manage/agent")){
     config.startUrl = window.location.pathname.slice(1, window.location.pathname.length);
     config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
     config.scope = "manage/agent"
@@ -9,11 +9,6 @@ function preinitializeProgressier(){
     config.startUrl = window.location.pathname.slice(1, window.location.pathname.length);
     config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
     config.scope = "manage/admin"
-  }
-  if (pathname.includes("/version-test")){
-    config.name = "[DIV] bizpwa";
-    config.shortName = "[DIV] bizpwa";
-    config.startUrl = window.location.pathname.slice(1, window.location.pathname.length);
   }
   window.progressierAppRuntimeSettings = config;
   let progressierScript = document.createElement("script");
