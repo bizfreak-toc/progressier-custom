7d2a7ce73b90cf1a43dcba94fc919f6238a1efff
徳長和希 <tokunagakazuki@tokuchoukazukinoMacBook-Air.local>
2024-06-12 19:16:55 +0900

    tst

--- bizpwa.js
+++ bizpwa.js
@@ -4,9 +4,11 @@ function preinitializeProgressier(){
   if (pathname.includes("/manage/agent")){
     config.startUrl = window.location.pathname.slice(1, window.location.pathname.length);
     config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
+    config.scope = "version-test/manage/agent"
   } else if (pathname.includes("/manage/admin")) {
     config.startUrl = window.location.pathname.slice(1, window.location.pathname.length);
     config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
+    config.scope = "version-test/manage/admin"
   }
   window.progressierAppRuntimeSettings = config;
   let progressierScript = document.createElement("script");
