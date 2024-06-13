f264ab3bb8c1345fdffafcbc6da092d9292185db
徳長和希 <tokunagakazuki@tokuchoukazukinoMacBook-Air.local>
2024-06-13 10:50:12 +0900

    リファクタ

--- bizpwa.js
+++ bizpwa.js
@@ -1,18 +1,30 @@
-function preinitializeProgressier(){
+function setConfig(config, basePath, scope) {
+  config.startUrl = basePath + scope;
+  config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
+  config.scope = basePath + scope;
+}
+
+function preinitializeProgressier() {
   let pathname = window.location.pathname;
   let config = {};
-  if (pathname.includes("/manage/agent")){
-    config.startUrl = "version-test/manage/agent";
-    config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
-    config.scope = "version-test/manage/agent";
-  } else if (pathname.includes("/manage/admin")) {
-    config.startUrl = "version-test/manage/admin";
-    config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
-    config.scope = "version-test/manage/admin";
+  let basePath = pathname.includes("version-test") ? "version-test/" : "";
+
+  if (pathname.includes("manage/agent")) {
+    setConfig(config, basePath, "manage/agent");
+  } else if (pathname.includes("manage/admin")) {
+    setConfig(config, basePath, "manage/admin");
   }
+
+  if (pathname.includes("version-test")) {
+    config.name = "[DEV] bizpwa";
+    config.shortName = "[DEV] bizpwa";
+  }
+
   window.progressierAppRuntimeSettings = config;
+
   let progressierScript = document.createElement("script");
   progressierScript.setAttribute("src", "https://progressier.app/tTD4oTBMz4ZuQAhT7HeQ/script.js");
   document.querySelector("head").appendChild(progressierScript);
 }
+
 preinitializeProgressier();
