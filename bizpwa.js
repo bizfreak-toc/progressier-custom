30c7d878f3bf59dba6bf290c059d06fa0fc5047f
徳長和希 <tokunagakazuki@tokuchoukazukinoMacBook-Air.local>
2024-06-13 11:19:57 +0900

    修正

--- bizpwa.js
+++ bizpwa.js
@@ -1,7 +1,7 @@
 function setConfig(config, basePath, scope, name, shortName) {
-  config.startUrl = scope;
+  config.startUrl = basePath + scope;
   config.icon512 = "https://pwa.xyz/v0/b/pwaa-8d87e.appspot.com/o/eWYX0PrgnbmJTIRgmYiH%2FiDWpnFeonBLfiQN.png?alt=media&token=7b7d4e52-4aac-46bc-bbe9-ece8bc6738f7";
-  config.scope = scope;
+  config.scope = basePath + scope;
   config.name = name;
   config.shortName = shortName;
 }
@@ -10,13 +10,13 @@ function preinitializeProgressier() {
   let pathname = window.location.pathname;
   let config = {};
   let basePath = pathname.includes("version-test") ? "version-test/" : "";
-  let namePrefix = basePath ? "[DEV] bizpwa" : "bizpwa";
-  let shortNamePrefix = basePath ? "[DEV] bizpwa" : "bizpwa";
+  let name = basePath ? "[DEV] bizpwa" : "bizpwa";
+  let shortName = basePath ? "[DEV] bizpwa" : "bizpwa";
 
   if (pathname.includes("manage/agent")) {
-    setConfig(config, basePath, "manage/agent", namePrefix + " Agent", shortNamePrefix + " Agent");
+    setConfig(config, basePath, "manage/agent", name, shortName);
   } else if (pathname.includes("manage/admin")) {
-    setConfig(config, basePath, "manage/admin", namePrefix + " Admin", shortNamePrefix + " Admin");
+    setConfig(config, basePath, "manage/admin", name, shortName);
   }
 
   window.progressierAppRuntimeSettings = config;
@@ -27,3 +27,4 @@ function preinitializeProgressier() {
 }
 
 preinitializeProgressier();
+
